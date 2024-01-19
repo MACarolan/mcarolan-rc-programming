d64dcebb1d2682f8b45e93f4fcb015e1d6d371ac
MACarolan <michaelcarolan12@gmail.com>
2024-01-18 23:34:41 -0500

    Created config_sample.py and .gitignore file
    The config file will contain secrets, so it is ignored. The config_sample is a guide

---
+++ config_sample.py
@@ -0,0 +1,10 @@
+'''
+DO NOT Commit
+'''
+API_KEY = 'YOUR_API_KEY'
+db_params = {
+    'host': 'localhost',
+    'database': 'ROYAL_CARIBBEAN',
+    'user': 'postgres',
+    'password': 'postgres'
+}
